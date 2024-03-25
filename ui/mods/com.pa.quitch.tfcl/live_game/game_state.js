5d744f415b8badf5a6a8a40c8546cc431d36f549
Quitch <Quitch@users.noreply.github.com>
2024-03-25 21:04:23 +0000

    Set chronocam time by spawn time

--- ui/mods/com.pa.quitch.tfcl/live_game/game_state.js
+++ ui/mods/com.pa.quitch.tfcl/live_game/game_state.js
@@ -8,14 +8,19 @@ function monitorGameState() {
   try {
     _.defer(function () {
       var liveGameOptionsBarPanelId = 1;
-
       liveGameOptionsBarPanelId = _.find(api.panelsById, {
         src: "coui://ui/main/game/live_game/live_game_options_bar.html",
+      }).id;
+
+      var liveGameTimeBarPanelId = 1;
+      liveGameTimeBarPanelId = _.find(api.panelsById, {
+        src: "coui://ui/main/game/live_game/live_game_time_bar.html",
       }).id;
 
       model.mode.subscribe(function () {
         if (model.mode() === "playing") {
           api.Panel.message(liveGameOptionsBarPanelId, "gameStarted");
+          api.Panel.message(liveGameTimeBarPanelId, "gameStarted");
         }
       });
     });
---
+++ ui/mods/com.pa.quitch.tfcl/live_game_time_bar/timer.js
@@ -0,0 +1,35 @@
+var timeFromCommanderLandingLoaded;
+
+function timeFromCommanderLanding() {
+  if (timeFromCommanderLandingLoaded) {
+    return;
+  }
+
+  timeFromCommanderLandingLoaded = true;
+
+  try {
+    var simStartToLandingTimeDifference = 0.0;
+
+    handlers.gameStarted = function () {
+      _.delay(function () {
+        simStartToLandingTimeDifference = model.endOfTimeInSeconds();
+      }, 5000);
+    };
+
+    model.currentTimeString = ko.computed(function () {
+      return UberUtility.createTimeString(
+        model.currentTimeInSeconds() - simStartToLandingTimeDifference
+      );
+    });
+
+    model.endOfTimeString = ko.computed(function () {
+      return UberUtility.createTimeString(
+        model.endOfTimeInSeconds() - simStartToLandingTimeDifference
+      );
+    });
+  } catch (e) {
+    console.error(e);
+    console.error(JSON.stringify(e));
+  }
+}
+timeFromCommanderLanding();
